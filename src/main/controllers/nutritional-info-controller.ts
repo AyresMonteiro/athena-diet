4d8b159b0ee46c8bacf2322425994e187c0c20f7
AyresMonteiro <ayresmonteiro52@gmail.com>
2023-01-08 16:50:13 -0300

    feat: 🎸 adds get() to NutritionalInfoController

--- src/main/controllers/nutritional-info-controller.ts
+++ src/main/controllers/nutritional-info-controller.ts
@@ -3,8 +3,10 @@ import {
   NutritionalInfoWithId,
 } from "../../domain/entities/nutritional-info";
 
-import { DbSaveNutritionalInfo } from "../../data/usecases/db-save-nutritional-info";
 import { INutritionalInfoRepository } from "../../data/repositories/nutritional-info-repository";
+
+import { DbGetNutritionalInfo } from "../../data/usecases/db-get-nutritional-info";
+import { DbSaveNutritionalInfo } from "../../data/usecases/db-save-nutritional-info";
 
 import { HTTPRequest } from "../../infra/http/http-request";
 import { HTTPResponse } from "../../infra/http/http-response";
@@ -27,6 +29,22 @@ export class NutritionalInfoController {
       const nutritionalInfo = await saveNutritionalInfo.execute(data);
 
       return { status: 201, data: nutritionalInfo };
+    } catch (e: unknown) {
+      return { status: 500, data: { message: "system error" } };
+    }
+  }
+
+  async get(
+    _req: HTTPRequest<null>
+  ): Promise<HTTPResponse<NutritionalInfoWithId[]>> {
+    try {
+      const getNutritionalInfo = new DbGetNutritionalInfo(
+        this.nutritionalInfoRepository
+      );
+
+      const nutritionalInfo = await getNutritionalInfo.execute();
+
+      return { status: 200, data: nutritionalInfo };
     } catch (e: unknown) {
       return { status: 500, data: { message: "system error" } };
     }
