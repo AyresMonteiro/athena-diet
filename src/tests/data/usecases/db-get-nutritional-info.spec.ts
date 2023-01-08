2bc6ed07bad355077d617684196d4a3d78dfb3e5
AyresMonteiro <ayresmonteiro52@gmail.com>
2023-01-08 16:49:54 -0300

    test: 💍 suite "@data/usecases/db-get-nutritional-info"

---
+++ src/tests/data/usecases/db-get-nutritional-info.spec.ts
@@ -0,0 +1,25 @@
+import { DbGetNutritionalInfo } from "../../../data/usecases/db-get-nutritional-info";
+import { IGetNutritionalInfo } from "../../../domain/usecases/get-nutritional-info";
+import { InMemoryNutritionalInfoRepository } from "../../../infra/in-memory/in-memory-nutritional-info-repository";
+
+import { nutritionalInfoMock } from "../../mock/nutritional-info.mock";
+
+describe("@data/usecases/db-get-nutritional-info", () => {
+  it("should get nutritional info", async () => {
+    const nutritionalInfoRepository = new InMemoryNutritionalInfoRepository();
+
+    const nutritionalInfo = nutritionalInfoMock();
+
+    await nutritionalInfoRepository.save(nutritionalInfo);
+
+    const getNutritionalInfo: IGetNutritionalInfo = new DbGetNutritionalInfo(
+      nutritionalInfoRepository
+    );
+
+    const results = await getNutritionalInfo.execute(null);
+
+    expect(results).not.toStrictEqual(undefined);
+    expect(Array.isArray(results)).toStrictEqual(true);
+    expect(results.length).toStrictEqual(1);
+  });
+});
