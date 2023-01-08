a6ffbcdc5ddf4bf1d891c71a964b026ac5c37d1e
AyresMonteiro <ayresmonteiro52@gmail.com>
2023-01-08 13:23:57 -0300

    test: 💍 should save nutritional info

---
+++ src/tests/domain/usecases/save-nutritional-info.spec.ts
@@ -0,0 +1,54 @@
+import { NutritionalInfo } from "../../../domain/entities/nutritional-info";
+import { nutritionalInfoMock } from "../../mock/nutritional-info.mock";
+
+interface IUsecase<DataType, ReturnType> {
+  execute(data: DataType): Promise<ReturnType>;
+}
+
+export type NutritionalInfoWithId = NutritionalInfo & { _id: string };
+
+interface ISaveNutritionalInfo
+  extends IUsecase<NutritionalInfo, NutritionalInfoWithId> {}
+
+interface INutritionalInfoRepository {
+  save(data: NutritionalInfo): Promise<NutritionalInfoWithId>;
+}
+
+export class DbSaveNutritionalInfo implements ISaveNutritionalInfo {
+  constructor(
+    private readonly nutritionalInfoRepository: INutritionalInfoRepository
+  ) {}
+
+  async execute(data: NutritionalInfo): Promise<NutritionalInfoWithId> {
+    return await this.nutritionalInfoRepository.save(data);
+  }
+}
+
+export class InMemoryNutritionalInfoRepository
+  implements INutritionalInfoRepository
+{
+  constructor(
+    private data: NutritionalInfoWithId[] = [],
+    private lastId: number = 0
+  ) {}
+
+  async save(data: NutritionalInfo): Promise<NutritionalInfoWithId> {
+    this.data.push({ ...data, _id: (++this.lastId).toString() });
+
+    return this.data[this.lastId - 1];
+  }
+}
+
+describe("@domain/usecases/save-nutritional-info", () => {
+  it("should save nutritional info", async () => {
+    const saveNutritionalInfo: ISaveNutritionalInfo = new DbSaveNutritionalInfo(
+      new InMemoryNutritionalInfoRepository()
+    );
+
+    const nutritionalInfo = nutritionalInfoMock();
+
+    const results = await saveNutritionalInfo.execute(nutritionalInfo);
+
+    expect(results._id).not.toStrictEqual(undefined);
+  });
+});
