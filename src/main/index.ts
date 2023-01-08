b65344fed8ce55f595fcdae84456dd39ca550ebb
AyresMonteiro <ayresmonteiro52@gmail.com>
2023-01-08 16:32:29 -0300

    feat: 🎸 creates project index

---
+++ src/main/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { InMemoryNutritionalInfoRepository } from "../infra/in-memory/in-memory-nutritional-info-repository";
+import { ExpressAdapter } from "./adapters/express-adapter";
+import { NutritionalInfoRouter } from "./routes/nutritional-info-router";
+
+const athena = express();
+
+const defaultAdapter = new ExpressAdapter(athena);
+
+const defaultNutritionalInfoRepository =
+  new InMemoryNutritionalInfoRepository();
+
+const routers = [
+  new NutritionalInfoRouter(defaultAdapter, defaultNutritionalInfoRepository),
+];
+
+for (const router of routers) {
+  router.register();
+}
+
+athena.listen(process.env.PORT ?? 3333);
+
+console.log("ATHENA is listening " + (process.env.PORT ?? 3333));
