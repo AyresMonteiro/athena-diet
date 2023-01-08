5f6e9a211cd577c2c8f79766823a43ed8a2ba7a0
AyresMonteiro <ayresmonteiro52@gmail.com>
2023-01-08 13:35:32 -0300

    refactor: 💡 moves InMemoryNutritionalInfoRepository
    moves InMemoryNutritionalInfoRepository to its own file

---
+++ src/infra/in-memory/in-memory-nutritional-info-repository.ts
@@ -0,0 +1,20 @@
+import { INutritionalInfoRepository } from "../../data/repositories/nutritional-info-repository";
+import {
+  NutritionalInfo,
+  NutritionalInfoWithId,
+} from "../../domain/entities/nutritional-info";
+
+export class InMemoryNutritionalInfoRepository
+  implements INutritionalInfoRepository
+{
+  constructor(
+    private data: NutritionalInfoWithId[] = [],
+    private lastId: number = 0
+  ) {}
+
+  async save(data: NutritionalInfo): Promise<NutritionalInfoWithId> {
+    this.data.push({ ...data, _id: (++this.lastId).toString() });
+
+    return this.data[this.lastId - 1];
+  }
+}
--- src/tests/domain/usecases/save-nutritional-info.spec.ts
+++ src/tests/domain/usecases/save-nutritional-info.spec.ts
@@ -1,24 +1,10 @@
-import { INutritionalInfoRepository } from "../../../data/repositories/nutritional-info-repository";
-import { DbSaveNutritionalInfo } from "../../../data/usecases/db-save-nutritional-info";
-import { NutritionalInfo, NutritionalInfoWithId } from "../../../domain/entities/nutritional-info";
 import { ISaveNutritionalInfo } from "../../../domain/usecases/save-nutritional-info";
 
+import { DbSaveNutritionalInfo } from "../../../data/usecases/db-save-nutritional-info";
+
+import { InMemoryNutritionalInfoRepository } from "../../../infra/in-memory/in-memory-nutritional-info-repository";
+
 import { nutritionalInfoMock } from "../../mock/nutritional-info.mock";
-
-export class InMemoryNutritionalInfoRepository
-  implements INutritionalInfoRepository
-{
-  constructor(
-    private data: NutritionalInfoWithId[] = [],
-    private lastId: number = 0
-  ) {}
-
-  async save(data: NutritionalInfo): Promise<NutritionalInfoWithId> {
-    this.data.push({ ...data, _id: (++this.lastId).toString() });
-
-    return this.data[this.lastId - 1];
-  }
-}
 
 describe("@domain/usecases/save-nutritional-info", () => {
   it("should save nutritional info", async () => {
